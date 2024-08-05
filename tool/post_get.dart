0bd14219253cd245b97705af2c74629196f8a100
Linh Nguyen <43808684+linhnguyen-gt@users.noreply.github.com>
2024-08-05 20:21:08 +0700

    feat: update build

--- tool/post_get.dart
+++ tool/post_get.dart
@@ -1,13 +1,23 @@
 // tool/post_get.dart
 import 'dart:io';
 
-void main() {
-  // Run `lefthook install`
-  Process.run('lefthook', ['install']).then((result) {
+void main() async {
+  try {
+    // Run `lefthook install`
+    final result = await Process.run('lefthook', ['install']);
+
     stdout.write(result.stdout);
     stderr.write(result.stderr);
+
     if (result.exitCode != 0) {
+      stderr.writeln(
+          'Lefthook installation failed with exit code ${result.exitCode}');
       exit(result.exitCode);
+    } else {
+      stdout.writeln('Lefthook installed successfully.');
     }
-  });
+  } catch (e) {
+    stderr.writeln('Failed to run lefthook: $e');
+    exit(1);
+  }
 }
